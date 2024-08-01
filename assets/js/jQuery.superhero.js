7f45aa7e280080a83cd034c678f1566b950a6b2f
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-01 16:19:54 -0400

    M4D10 prueba: Refactored script.js to remove fetchHeroeData and renderHeroeCard functions, replaced with a call to a new jQuery plugin 'superhero' and added a script tag for jQuery.superhero.js in index.html

---
+++ assets/js/jQuery.superhero.js
@@ -0,0 +1,42 @@
+jQuery.fn.superhero = function (token, idHeroe) {
+    // acceder al ajax mediante jquery
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://superheroapi.com/api.php/${token}/${idHeroe}`,
+        "method": "GET",
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        renderHeroeCard(response);
+    });
+
+    // renderizar los datos del api
+    function renderHeroeCard(data) {
+        const cardHtml = `
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4 d-flex align-items-center">
+                    <img src="${data.image.url}" class="img-fluid h-100 object-fit-cover" alt="Foto de perfil del héroe ${data.name}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Nombre: ${data.name}</h5>
+                        <p class="card-text">Conexiones: ${data.connections['group-affiliation']} </p>
+                        <p class="card-text">Publicado por: ${data.biography['publisher']}</p>
+                        <p class="card-text">Ocupación: ${data.work['occupation']}</p>
+                        <p class="card-text">Primera aparición: ${data.biography['first-appearance']}</p>
+                        <p class="card-text">Altura: ${data.appearance['height']}</p>
+                        <p class="card-text">Peso: ${data.appearance['weight']}</p>
+                        <p class="card-text">Alianzas: ${data.biography['aliases']}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+        $('#resultadoTitle').show();
+        $('#resultadoHero').html(cardHtml);
+    }
+    return this;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $("#btnHeroe").click(function (e) {
         e.preventDefault();
-        let numHeroe = getHeroeID();
-        if (numHeroe !== null) {
-            fetchHeroeData(numHeroe);
-        } else {
+        let idHeroe = getHeroeID();
+        if (idHeroe === null) {
             alert("Reintenta con un dígito numérico por favor");
+        } else {
+            let token = "d99f24a4825f426f9b0b4b4410645787";
+            $("#resultadoHero").superhero(token, idHeroe);
         }
     });
 });
@@ -13,52 +14,10 @@ $(document).ready(function () {
 // validacion tipo numérico
 function getHeroeID() {
     const regex = /^\d+$/;
-    let numHeroe = $("#inputHeroe").val();
-    if (regex.test(numHeroe)) {
-        return numHeroe;
+    let idHeroe = $("#inputHeroe").val();
+    if (regex.test(idHeroe)) {
+        return idHeroe;
     } else {
         return null;
     }
-}
-
-// acceder al ajax mediante jquery
-function fetchHeroeData(numHeroe) {
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://superheroapi.com/api.php/d99f24a4825f426f9b0b4b4410645787/${numHeroe}`,
-        "method": "GET",
-    };
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        renderHeroeCard(response);
-    });
-}
-
-// renderizar los datos del api
-function renderHeroeCard(data) {
-    const cardHtml = `
-    <div class="card mb-3">
-        <div class="row g-0">
-            <div class="col-md-4 d-flex align-items-center">
-                <img src="${data.image.url}" class="img-fluid h-100 object-fit-cover" alt="Foto de perfil del héroe ${data.name}">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Nombre: ${data.name}</h5>
-                    <p class="card-text">Conexiones: ${data.connections['group-affiliation']} </p>
-                    <p class="card-text">Publicado por: ${data.biography['publisher']}</p>
-                    <p class="card-text">Ocupación: ${data.work['occupation']}</p>
-                    <p class="card-text">Primera aparición: ${data.biography['first-appearance']}</p>
-                    <p class="card-text">Altura: ${data.appearance['height']}</p>
-                    <p class="card-text">Peso: ${data.appearance['weight']}</p>
-                    <p class="card-text">Alianzas: ${data.biography['aliases']}</p>
-                </div>
-            </div>
-        </div>
-    </div>`;
-
-    $('#resultadoTitle').show();
-    $('#resultadoHero').html(cardHtml);
 }
