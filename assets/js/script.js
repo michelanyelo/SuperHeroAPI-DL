9d13d7070900efc6946b5306108fcf13ba749d38
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-01 12:38:37 -0400

    M4D10 prueba: 3.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
     $("#btnHeroe").click(function (e) {
         e.preventDefault();
-        let numHeroe = $("#inputHeroe").val();
-        console.log(numHeroe);
+        let numHeroe = getHeroeID();
+        fetchHeroeData(numHeroe);
     });
 });
+
+function getHeroeID() {
+    return $("#inputHeroe").val();
+}
+
+function fetchHeroeData(numHeroe) {
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://superheroapi.com/api.php/d99f24a4825f426f9b0b4b4410645787/${numHeroe}`,
+        "method": "GET",
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
