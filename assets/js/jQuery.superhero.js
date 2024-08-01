4321a9b7781680097efaf7acfcd8da55da8c9cae
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-01 19:25:19 -0400

    M4D10 prueba: 3.7

--- assets/js/jQuery.superhero.js
+++ assets/js/jQuery.superhero.js
@@ -10,9 +10,10 @@ jQuery.fn.superhero = function (token, idHeroe) {
     };
 
     $.ajax(settings).done(function (response) {
-        console.log(response);
         renderHeroeCard(element, response);
+        renderChart(response);
     });
+
     return this;
 }
 
@@ -21,28 +22,23 @@ function renderHeroeCard(element, response) {
     // Array de objetos con claves y valores para los atributos del superheroe
     const heroAttributes = [
         { label: 'Conexiones', value: response.connections['group-affiliation'] },
-        { label: 'Publicado por', value: response.biography['publisher'] },
         { label: 'Ocupación', value: response.work['occupation'] },
         { label: 'Primera aparición', value: response.biography['first-appearance'] },
         { label: 'Alianzas', value: response.biography['aliases'].join(', ') },
-        // Nuevos arrays sin espacios entre valor y lb/kg y valor pulgadas/cm para los items de Altura y Peso
-        // '183 cm' se convierte en '183cm'
-        // Uso de join para retornar un string separados por guion (-)
-        // 6ft - 183cm.
         { label: 'Altura', value: response.appearance['height'].map(height => height.replace(' ', '')).join(' - ') },
         { label: 'Peso', value: response.appearance['weight'].map(weight => weight.replace(' ', '')).join(' - ') }
     ];
 
     // Crear el HTML para la tarjeta del superheroe (como eran muchos parrafos los procesé con map)
     const cardHtml = `
-        <div class="card mb-3">
+        <div class="card mb-3 border-0">
             <div class="row g-0">
                 <div class="col-md-4 d-flex align-items-center">
                     <img src="${response.image.url}" class="img-fluid h-100 object-fit-cover" alt="Foto de perfil del superheroe ${response.name}">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">Nombre: ${response.name}</h5>
+                        <h5 class="card-title">${response.name} <small class="text-body-secondary"> ${response.biography['publisher']} </small></h5>
                         ${heroAttributes.map(attr => `
                             <p class="card-text">${attr.label}: ${attr.value}</p>
                         `).join('')}
@@ -52,7 +48,48 @@ function renderHeroeCard(element, response) {
         </div>`;
 
     // Usar `element` para mostrar el contenido y la tarjeta del superheroe
-    console.log("Card HTML:", element);
     $("#resultadoTitle").show();
     element.html(cardHtml);
+}
+
+// Función para renderizar los datos del superhéroe en un gráfico
+function renderChart(response) {
+    const chartData = [];
+    const stats = response.powerstats;
+
+    // Recopilar datos para el gráfico
+    for (const power in stats) {
+        if (stats[power] !== "null") {
+            chartData.push({ label: power, y: Number(stats[power]) });
+        }
+    }
+
+    // Verificar si hay datos para mostrar en el gráfico
+    if (chartData.length === 0) {
+        // Mostrar mensaje e imagen si no hay datos
+        $("#chartContainer").html(`
+            <p class='text-danger fw-bold'>Este héroe no tiene poderes registrados :(</p>
+            <img class='img-fluid w-75 yamcha' src='assets/img/yamcha.jpg' alt='Imagen de Yamcha'>
+        `);
+    } else {
+        // Configurar y renderizar el gráfico si hay datos
+        let chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light1", // "light1", "light2", "dark1", "dark2"
+            animationEnabled: true,
+            title: {
+                text: `Stats de ${response.name}`
+            },
+            data: [{
+                type: "pie",
+                startAngle: 25,
+                toolTipContent: "<b>{label}</b>: {y}%",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - {y}%",
+                dataPoints: chartData
+            }]
+        });
+        chart.render();
+    }
 }
